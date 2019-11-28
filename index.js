7d636a5b059529bf1b80c7ebc85abc3f5a04f9d7
Jean Devalance <=>
2019-11-28 11:11:55 +0100

    Error handling

--- index.js
+++ index.js
@@ -15,40 +15,36 @@ const db = new sqlite3.Database(dbFile, (err) => {
 
 const saveReadable = (id, content) => {
   db.run(
-    `UPDATE expression SET readable = ? WHERE id = ?`,
+    'UPDATE expression SET readable = ? WHERE id = ?',
     [content, id],
-    err => { if (err) throw err; }
+    err => {
+      if (err) {
+        console.log(util.format('Error : %s on processing expression #%s', err.code, id));
+      } else {
+        byteSize = Buffer.from(content).length;
+        console.log(util.format('Saved %s bytes from expression readable #%s', byteSize, id));
+      }
+    }
   );
 };
 
 db.serialize(() => {
-  const sql = util.format(`
-    SELECT
-      id,
-      url,
-      readable
-    FROM expression
-    WHERE land_id = %s
-      AND relevance >= %s
-    LIMIT %s
-    OFFSET %s`,
-    landId,
-    minRelevance,
-    pageLimit,
-    pageOffset
+  const sql = util.format(
+    'SELECT id, url, readable FROM expression WHERE land_id = %s AND relevance >= %s LIMIT %s OFFSET %s',
+    landId, minRelevance, pageLimit, pageOffset
   );
   
   db.each(sql, (err, row) => {
     if (err) throw err;
     console.log(util.format('Processing expression #%s', row.id));
-
-    Mercury.parse(row.url, {
+ 
+ Mercury.parse(row.url, {
       contentType: 'markdown',
     })
     .then(result => {
-      byteSize = Buffer.from(result.content).length;
       saveReadable(row.id, result.content);
-      console.log(util.format('Saved %s bytes from expression readable #%s', byteSize, row.id));
+    }).catch(err => {
+      console.log(err);
     });
   });
 });
